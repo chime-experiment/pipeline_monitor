1779634297d9f32883bd2017bf6201024bef7b95
ljgray <liam.j.gray@usask.ca>
2022-07-05 17:48:24 -0700

    feat(metrics): small service to track remote pipeline metrics.
    Fetches chp metrics from a remote source and exposes them to prometheus.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-pipeline_monitor
+Flask web app and Prometheus config to monitor CHIME pipeline progress. 
+
+# Install
+Installation should be done in a virtual environment.
+```console
+(venv) foo@bar:~$ pip install -r <path-to-file>/requirements.txt
+```
+Config must be set up by making a copy of config/sample_config.yml.
+
+
+# Running
+```console
+(venv) foo@bar:~$ ./run.sh
+```
+
+If Prometheus is running as a system service, make sure to add the job found in 'config/prometheus.yml' to the prometheus config file. Otherwise, run
+```console
+(venv) foo@bar:~$ sudo /<path-to-prometheus> --config.file=pipeline_monitor/config/prometheus.yml
---
+++ pipeline_monitor/app.py
@@ -0,0 +1,67 @@
+import yaml
+import atexit
+from datetime import datetime
+from pathlib import Path
+from functools import partial
+
+from flask import Flask, g
+from prometheus_client import make_wsgi_app
+from apscheduler.schedulers.background import BackgroundScheduler
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+from pipeline_monitor import commands
+
+# SSH config file
+_CONFIG_FILE = "./pipeline_monitor/config/ssh_config.yml"
+# Prometheus metrics scrape path. Must match metrics_path
+# for job set in prometheus config file.
+_PROMETHEUS_METRICS_TARGET = "/metrics"
+
+# Flask app to provide minimal endpoint for prometheus
+app = Flask(__name__)
+# Add prometheus wsgi middleware to route requests
+app.wsgi_app = DispatcherMiddleware(
+    app.wsgi_app, {_PROMETHEUS_METRICS_TARGET: make_wsgi_app()}
+)
+
+
+def get_config() -> dict:
+    """Get config. If none exists, load
+    from yaml file."""
+
+    config = getattr(g, "_config", None)
+    if not config:
+        config = g._config = _get_yaml_config()
+
+    return config
+
+
+def _get_yaml_config() -> dict:
+    """Load yaml config file. Expects global variable
+    _CONFIG_FILE to exist."""
+
+    if not Path(_CONFIG_FILE).exists():
+        raise FileNotFoundError(
+            f"No config file found. Expecting a config file '{_CONFIG_FILE}'"
+        )
+    with (Path(_CONFIG_FILE)).open() as fh:
+        return yaml.safe_load(fh)
+
+
+# Start background task to periodically fetch metrics.
+# Task will run for the first time immediately.
+# In order for this to work, uwsgi server must be
+# run with threads enabled.
+scheduler = BackgroundScheduler(daemon=True)
+with app.app_context():
+    # Job has to be created with app context in order to
+    # access flask 'g' variable where the config is stored
+    scheduler.add_job(
+        partial(commands.fetch_chp_metrics, config=get_config()),
+        "interval",
+        minutes=get_config()["frequency"],
+        next_run_time=datetime.now(),
+    )
+scheduler.start()
+# Shut down the scheduler when flask app closes
+atexit.register(lambda: scheduler.shutdown())
