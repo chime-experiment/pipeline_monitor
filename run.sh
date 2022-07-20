5f4368b0af5b93d14f7b47efd99faf9bb5908ab3
ljgray <liam.j.gray@usask.ca>
2022-07-20 11:04:39 -0700

    add(config): add ssh_config file

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-uwsgi --http localhost:5000 --wsgi-fil pipeline_monitor/app.py --callable app --enable-threads
+uwsgi --http localhost:5000 --wsgi-file pipeline_monitor/app.py --callable app --enable-threads
